fc72c2a574c6ec3a08387749822be5a58dac9deb
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-10-18 15:44:46 +0100

    returning Median

--- median.py
+++ median.py
@@ -1,13 +1,19 @@
 # hello first commit 
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
+        try:
+            print("Enter a list of numbers separated by commas: ")
+            numbers = [float(value) for value in input().split(",")]
+            numbers.sort()
+            n = len(numbers)
+            if n % 2 == 1:
+                Median = (numbers[n//2])
+            else:
+               Median = (numbers[n//2] + numbers[(n//2)-1])/2)
+        except ValueError:
+            print("Some input could not be converted to a number!")
+            
 print(numbers)
+print(Median)
+
