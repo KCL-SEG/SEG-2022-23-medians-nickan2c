71b1abdee29f5a56ede19d3aa338e7ea87425643
Nickan Tousi <nickan.tousi@kcl.ac.uk>
2022-10-21 16:22:12 +0100

    Change so that loop does not run indefinitely

--- median.py
+++ median.py
@@ -1,19 +1,20 @@
-# hello first commit 
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 while True:
         try:
             print("Enter a list of numbers separated by commas: ")
             numbers = [float(value) for value in input().split(",")]
-            numbers.sort()
-            n = len(numbers)
-            if n % 2 == 1:
-                Median = (numbers[n//2])
-            else:
-                Median =((numbers[n//2] + numbers[(n//2)-1])/2)
+            
         except ValueError:
             print("Some input could not be converted to a number!")
-            
-print(numbers)
-print(Median)
+        else:
+            break
 
+numbers.sort()
+n = len(numbers)
+if n % 2 == 1:
+    Median = (numbers[n//2])
+else:
+    Median =((numbers[n//2] + numbers[(n//2)-1])/2)
+
+print(Median)
